610dcb3b2edb80c9c85ff0cbb96adc1883e3017e
Papryk <gustaw5022@gmail.com>
2020-07-01 23:17:17 +0200

    added basics

--- PlayBox/app/src/main/java/com/partos/playbox/MainActivity.kt
+++ PlayBox/app/src/main/java/com/partos/playbox/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.partos.playbox
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.partos.playbox.fragments.MainMenuFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    MainMenuFragment.OnFragmentInteractionListener {
+
+    private lateinit var mainMenuFragment: MainMenuFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainMenuFragment = MainMenuFragment.newInstance()
+
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(
+                R.anim.enter_bottom_to_top, R.anim.exit_top_to_bottom,
+                R.anim.enter_top_to_bottom, R.anim.exit_bottom_to_top
+            )
+            .add(R.id.main_frame_layout, mainMenuFragment)
+            .commit()
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+
     }
 }
