db77839f515cf7d87259594e14f200d1cea7720d
Papryk <gustaw5022@gmail.com>
2020-07-22 20:47:13 +0200

    added empty fragments

---
+++ PlayBox/app/src/main/java/com/partos/playbox/logic/pictionary/PictionaryTeamLogic.kt
@@ -0,0 +1,11 @@
+package com.partos.playbox.logic.pictionary
+
+import android.view.View
+import androidx.fragment.app.FragmentManager
+
+class PictionaryTeamLogic(val rootView: View, val fragmentManager: FragmentManager) {
+
+    fun initFragment(){
+
+    }
+}
